32a5f062db15d6b22c3ed187271caafa0c7d0cf9
Isa Gul <isa.gul@beyn.com.tr>
2020-01-23 17:19:41 +0300

    url loader was added for fonts instead of file-loader

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -38,7 +38,7 @@ module.exports = {
             test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
             use: [
               {
-                loader: 'file-loader',
+                loader: 'url-loader',
               }
             ]
           },
